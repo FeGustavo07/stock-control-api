59be015073120cd1cdd5c2e05c2fad8093599f55
Gustavo <gusfeh07@gmail.com>
2022-04-24 16:05:19 -0300

    fix: modules config for TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { UserModule } from './modules/user.module';
 import { ProductModule } from './modules/product.module';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ProductModule, UserModule],
+  imports: [ProductModule, UserModule, TypeOrmModule.forRoot()],
 })
 export class AppModule {}
