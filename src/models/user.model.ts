6076953bd7fa4466994d003e4181b6851f034de3
Gustavo <gusfeh07@gmail.com>
2022-04-24 15:15:47 -0300

    feat: create user model

---
+++ src/models/user.model.ts
@@ -0,0 +1,17 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class UserModel {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    position: string;
+
+    @Column({ length: 120 })
+    name: string;
+
+    @Column({ length: 80 })
+    password: string;
+}
