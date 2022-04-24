b487f6b60aa03d51e8d9e9fdfe443c6faad67224
Gustavo <gusfeh07@gmail.com>
2022-04-24 16:18:34 -0300

    feat: create ORM config file

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,11 +1,15 @@
-import { Controller, Delete, Get, Post, Put } from "@nestjs/common";
+import { ProductModel } from './../models/product.model';
+import { ProductService } from './../services/product.service';
+import { Body, Controller, Delete, Get, Post, Put } from "@nestjs/common";
 
 @Controller('/product')
 export class ProductController {
 
+    constructor(private service: ProductService) {}
+
     @Post()
-    public create() {
-        return {data: 'Created'}
+    public create(@Body() product: ProductModel) {
+        return this.service.create(product)
     }
 
     @Get()
